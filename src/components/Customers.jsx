81ea2f3a3112e64cd496f105c4bc2fc8fe0cf625
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-17 02:31:49 -0700

    width for mobile fixed

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -18,26 +18,26 @@ const Customers = () => {
 						keep up-and stay open.
 					</h3>
 					{/*mobile text*/}
-					<h3 className="text-[24px] leading-[46px] xl:hidden mt-[-130px]">
+					<h3 className="text-[24px] leading-[46px] xl:hidden mt-[-130px] px-[20px]">
 						We connect our customers with the best, and help them keep up-and
 						stay open.
 					</h3>
 				</div>
 
 				<div className="px-[20px] xl:px-0">
-					<div className="bg-[#0A2640] xl:w-[500px] h-[58px] w-[400px] rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
+					<div className="bg-[#0A2640] xl:w-[500px] h-[58px] w-[300px] rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
 						<img className="absolute" src={star} alt="feather logo" />
 						<p className="text-white relative left-[50px] leading-[28px] text-[16px]">
 							We connect our customers with the best.
 						</p>
 					</div>
-					<div className="xl:w-[500px] h-[58px] w-[400px] shadow-md shadow-slate-200 rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
+					<div className="xl:w-[500px] h-[58px] w-[300px] shadow-md shadow-slate-200 rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
 						<img className="absolute" src={starr} alt="feather logo" />
 						<p className=" relative left-[50px] leading-[28px] text-[16px]">
 							Advisor success customer launch party.
 						</p>
 					</div>
-					<div className="xl:w-[500px] h-[58px] w-[400px] shadow-md shadow-slate-200 rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
+					<div className="xl:w-[500px] h-[58px] w-[300px] shadow-md shadow-slate-200 rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out mt-2 px-2">
 						<img className="absolute" src={sun} alt="feather logo" />
 						<p className=" relative left-[50px] leading-[28px] text-[16px]">
 							Business-to-consumer long tail.
