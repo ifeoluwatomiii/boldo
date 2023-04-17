95230318df6ea8d07edcdd93e1d8d33c878427d7
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-17 01:15:41 -0700

    customers component made mobile responsive

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -8,14 +8,16 @@ const Connect = () => {
 			<div className="mt-[-50px] xl:mt-0">
 				<img src={phone} alt="man on phone and a graph" />
 			</div>
-			<div className="w-[450px] px-[20px]"> 
+			<div className="w-[450px] px-[20px] xl:px-0">
+				{/*desktop text*/}
 				<h3 className="text-[36px] leading-[56px] hidden xl:flex grow">
-					We connect our customers <br /> with the best, and help them <br />{" "}
+					We connect our customers <br /> with the best, and help them <br />
 					keep up-and stay open.
 				</h3>
-				<h3 className="text-[26px] leading-[46px] px-[20px] xl:hidden">
-				We connect our customers with the best, and help them
-					keep up-and stay open.
+				{/*mobile text*/}
+				<h3 className="text-[26px] leading-[46px] px-[25px] xl:hidden">
+					We connect our customers with the best, and help them keep up-and stay
+					open.
 				</h3>
 				<div className="mt-[40px] px-[14px]">
 					<div className="xl:w-[500px] xl:h-[58px] shadow-md shadow-slate-200 rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
@@ -27,17 +29,21 @@ const Connect = () => {
 					<div className="mt-[24px] xl:w-[500px] xl:h-[58px] shadow-md shadow-slate-200 rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
 						<img className="absolute" src={icon} alt="" />
 						<p className="relative left-[60px] xl:text-[20px] leading-[32px] text-[16px]">
-                            Advisor success customer launch party.
+							Advisor success customer launch party.
 						</p>
 					</div>
 					<div className="mt-[24px] xl:w-[500px] xl:h-[58px] shadow-md shadow-slate-200 rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
 						<img className="absolute" src={icon} alt="" />
 						<p className="relative left-[60px] xl:text-[20px] leading-[32px] text-[16px]">
-                            Business-to-consumer long tail.
+							Business-to-consumer long tail.
 						</p>
 					</div>
 				</div>
-                <button className="bg-[#0A2640]  text-white text-[20px] leading-[28px] rounded-full py-4 px-12 mt-[56px] hover:animate-bounce">Start Now</button>
+				<div className="px-[20px]">
+					<button className="bg-[#0A2640]  text-white text-[20px] leading-[28px] rounded-full py-4 px-12 mt-[56px] hover:animate-bounce">
+						Start Now
+					</button>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -6,25 +6,33 @@ import woman from "../assets/woman.png";
 
 const Customers = () => {
 	return (
-		<div className="mt-[206px] flex justify-center items-center gap-[150px]">
+		<div className="mt-[206px] flex justify-center items-center gap-[150px] flex-col xl:flex-row px-[20px]">
+			<div>
+				<img className="xl:hidden" src={woman} alt="" />
+			</div>
 			<div className="flex flex-col gap-[24px]">
-				<h3 className="leading-[56px] text-[36px]">
-					We connect our customers <br /> with the best, and help them <br /> keep up-and stay
-					open.
+				{/*desktop text*/}
+				<h3 className="leading-[56px] text-[36px] hidden xl:flex grow">
+					We connect our customers <br /> with the best, and help them <br />{" "}
+					keep up-and stay open.
 				</h3>
-				<div className="bg-[#0A2640] w-[500px] h-[58px] rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
+				{/*mobile text*/}
+				<h3 className="text-[26px] leading-[46px] px-[20px] xl:hidden mt-[-130px]">
+					We connect our customers with the best, and help them keep up-and stay open.
+				</h3>
+				<div className="bg-[#0A2640] xl:w-[500px] xl:h-[58px] px-[34px] rounded py-4 xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
 					<img className="absolute" src={star} alt="feather logo" />
 					<p className="text-white relative left-[50px] leading-[28px] text-[16px]">
 						We connect our customers with the best.
 					</p>
 				</div>
-				<div className="w-[500px] h-[58px] shadow-md shadow-slate-200 rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
+				<div className="xl:w-[500px] xl:h-[58px] shadow-md shadow-slate-200 rounded py-4 px-[34px] xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
 					<img className="absolute" src={starr} alt="feather logo" />
 					<p className=" relative left-[50px] leading-[28px] text-[16px]">
 						Advisor success customer launch party.
 					</p>
 				</div>
-				<div className="w-[500px] h-[58px] shadow-md shadow-slate-200 rounded py-4 px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
+				<div className="xl:w-[500px] xl:h-[58px] shadow-md shadow-slate-200 rounded py-4 px-[34px] xl:px-4 transition transform hover:hover:scale-110 duration-500 ease-in-out">
 					<img className="absolute" src={sun} alt="feather logo" />
 					<p className=" relative left-[50px] leading-[28px] text-[16px]">
 						Business-to-consumer long tail.
@@ -32,7 +40,11 @@ const Customers = () => {
 				</div>
 			</div>
 			<div>
-				<img src={woman} alt="woman on the telephone" />
+				<img
+					className="hidden xl:flex grow items-center justify-center"
+					src={woman}
+					alt="woman on the telephone"
+				/>
 			</div>
 		</div>
 	);
