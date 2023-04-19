2a27bf8bea05df438d501bf4d3085838f000fbf9
ifemi27 <opatolaifeoluwav@gmail.com>
2023-04-18 23:21:36 -0700

    boldo logo added to the sections

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ServicesCard from "./ServicesCard";
+import boldo from "../assets/b.svg"
 
 const Services = () => {
 	return (
 		<>
 			<div className="flex flex-col justify-center items-center py-[82px] px-[20px]">
+				<div className="flex justify-center items-center gap-3">
+				<img src={boldo} alt="boldo logo symbol" />
 				<p className="text-[#777777]">Our Services</p>
+				</div>
+				
 				<h2 className="leading-[72px] text-[48px] text-center hidden xl:flex grow">
 					Handshake infographic mass market <br /> crowdfunding iteration.
 				</h2>
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -4,8 +4,7 @@ import testimonial2 from "../assets/testimonial2.png";
 import testimonial3 from "../assets/testimonial3.png";
 import { useState } from "react";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
-
-
+import boldo from "../assets/b.svg";
 
 const Template = () => {
 	const [carousel, setCarousel] = useState(0);
@@ -22,40 +21,50 @@ const Template = () => {
 
 	return (
 		<div className="bg-[#0A2640] flex flex-col justify-center items-center py-[96px] px-[20px]">
+			<div className="flex justify-center items-center gap-3">
+				<img src={boldo} alt="boldo logo symbol" />
+				<p className="text-white">Template</p>
+			</div>
 			{/*desktop text*/}
 			<div className="mt-[96px] text-[48px] leading-[72px] hidden xl:flex grow">
 				<h3 className="text-white text-[48px] leading-[72px] text-center">
 					An enterprise template to ramp <br /> up your company website
 				</h3>
-				
 			</div>
 
 			{/*mobile text*/}
-			<div className="xl:hidden">
-			<h3 className="text-white text-[26px] leading-[46px] px-[20px] text-center">
-				An enterprise template to ramp up your company website
+			<div className="xl:hidden mt-4">
+				<h3 className="text-white text-[26px] leading-[46px] px-[20px] text-center">
+					An enterprise template to ramp up your company website
 				</h3>
-				
 			</div>
 			<div className="flex justify-center items-center mt-[52px] xl:mt-[72px] flex-col xl:flex-row">
 				<GrFormPrevious
 					className="w-[50px] h-[50px]  bg-white rounded-full cursor-pointer hidden xl:flex grow"
 					onClick={handlePrev}
 				/>
-                
-				<img className="h-[394px] w-[400px]" src={images[carousel]} alt={`Image ${carousel + 1}`} />
-                
+
+				<img
+					className="h-[394px] w-[400px]"
+					src={images[carousel]}
+					alt={`Image ${carousel + 1}`}
+				/>
 
 				<GrFormNext
 					className="w-[50px] h-[50px] bg-white rounded-full cursor-pointer hidden xl:flex grow"
 					onClick={handleNext}
 				/>
 				<div className="flex gap-5 xl:hidden">
-				<GrFormPrevious className="w-[50px] h-[50px] bg-white rounded-full cursor-pointer" onClick={handlePrev}/>
-				<GrFormNext className="w-[50px] h-[50px] bg-white rounded-full cursor-pointer" onClick={handleNext}/>
+					<GrFormPrevious
+						className="w-[50px] h-[50px] bg-white rounded-full cursor-pointer"
+						onClick={handlePrev}
+					/>
+					<GrFormNext
+						className="w-[50px] h-[50px] bg-white rounded-full cursor-pointer"
+						onClick={handleNext}
+					/>
 				</div>
 			</div>
-           
 		</div>
 	);
 };
